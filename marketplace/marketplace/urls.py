9daa16ef936a9d1dfc1c70ef2556afd3353a0767
Svetlana Kudinova <kudinovabis@gmail.com>
2021-03-25 14:45:09 +0300

    Queries

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
+from marketplace import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
