2a8dc6a0a789e1048a4bf887850e9f598c80f131
Svetlana Kudinova <kudinovabis@gmail.com>
2021-04-02 09:15:48 +0300

    Валидация возраста

--- marketplace/main/forms.py
+++ marketplace/main/forms.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
+from django.core.exceptions import ValidationError
 from django.forms.models import inlineformset_factory
 from django.forms.models import BaseInlineFormSet
 from django.forms import TextInput, EmailInput, formset_factory
@@ -11,7 +15,27 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('birth_date',)
+
+    def __init__(self, *args, user, **kwargs):
+        self.fields['birth_date'] = forms.DateField(widget=forms.SelectDateWidget(), label='Дата рождения')
+
+class ProfileFormset (forms.inlineformset_factory(User, Profile, fields=('birth_date',), can_delete=False)):
+    def __init__(self, *args, **kwargs):
+        self.__initial = kwargs.pop('initial', [])
+        super(ProfileFormset, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        super(ProfileFormset, self).clean()
+
+        for form in self.forms:
+            data = form.cleaned_data['birth_date']
+            birth_date_delta = datetime.now().date().year - data.year
+            if birth_date_delta < 18:
+                form.add_error('birth_date', 'Сайт доступен от 18 лет')
+                raise forms.ValidationError('Доступ ограничен !')
+            return data
