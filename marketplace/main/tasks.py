6ad486caa3f953d0df0db302a30cffec24c43b4f
Svetlana Kudinova <kudinovabis@gmail.com>
2021-04-14 11:26:27 +0300

    Периодическое задание

--- marketplace/main/tasks.py
+++ marketplace/main/tasks.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
-from celery.decorators import task
+
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from main.models import Good, Subscriber
 from marketplace.celery import app
@@ -31,4 +34,30 @@ def send_mail_about_new_good(good_id):
         logger.warning("Email отправлен")
 
     logger.info("Отправка email:Окончание таски")
+    return True
+
+
+@app.task
+def monday_mail_about_new_goods():
+    logger.info("Отправка email в понедельник:Запуск таски")
+    date_minus_7days = timezone.now() - timedelta(days=7)
+    goods_per_week = Good.objects.filter(date_create__gte=date_minus_7days)
+    from_email = 'admin@marketplace.ru'
+    subject = 'Новые товары на сайте с %s' % date_minus_7days
+    html_content = '<p><i>Здравствуйте</i></p>'
+    html_content += 'Новые товары:'
+
+    for goodone in goods_per_week:
+        title = goodone.good_name
+        html_content += '<a href="http://127.0.0.1:8000/main/goods/%s">%s</a>' % (goodone.id, title)
+
+    for subscrib_user in Subscriber.objects.all():
+        user = User.objects.get(id=subscrib_user.user_id)
+        to_email = user.email
+        email = EmailMessage(subject, html_content, from_email, [to_email])
+        email.content_subtype = "html"
+        email.send()
+        logger.warning("Email отправлен")
+
+    logger.info("Отправка email в понедельник:Окончание таски")
     return True
--- marketplace/marketplace/celery.py
+++ marketplace/marketplace/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
@@ -15,6 +16,15 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 12:00 a.m.
+    'add-every-monday-morning': {
+        'task': 'main.tasks.monday_mail_about_new_goods',
+        'schedule': crontab(minute='0', hour='12', day_of_week='sun')
+    },
+}
 
 
 @app.task(bind=True)
