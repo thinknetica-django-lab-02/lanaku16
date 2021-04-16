c904cea26ade3fee188b74bb82696f39c6c8b122
Svetlana Kudinova <kudinovabis@gmail.com>
2021-04-16 14:39:53 +0300

    Настройка redis cache

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -125,7 +125,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
